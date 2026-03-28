7eed00ac0aff1baa4ba0e704de219fefb8eac680
eslam.badawy <eslam.badawy@ESLAM-BADAWY>
2026-03-28 22:18:17 +0200

    Fix Chrome crash in CI

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,12 +2,31 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
     public static WebDriver initDriver() {
+
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 🔥 Detect if running in CI
+        String headless = System.getProperty("headless");
+
+        if ("true".equalsIgnoreCase(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            System.out.println("Running in HEADLESS mode (CI)");
+        } else {
+            System.out.println("Running in NORMAL mode (Local)");
+        }
+
+        return new ChromeDriver(options);
     }
 }
